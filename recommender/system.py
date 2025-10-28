1e35fc18bd0b54c619e39cefa83505b35a23b960
runying <67864475+Yevsquant@users.noreply.github.com>
2025-10-28 15:42:22 -0500

    Applied ItemCF in system

--- recommender/system.py
+++ recommender/system.py
@@ -4,17 +4,19 @@ from .data import build_feature_row
 from .candidate import candidate_generation
 from .rerank import rerank_diversity
 from recommender.retrieval.item_cf import ItemCF
-from recommender.ranker.multitask_ranker import MultiObjectiveRanker
 
 class RecommenderSystem:
-    def __init__(self, meta, ranker_model: MultiObjectiveRanker, item_cf: ItemCF):
+    def __init__(self, meta, ranker_model, item_cf: ItemCF):
         self.meta = meta
         self.ranker =ranker_model
         self.item_cf = item_cf
     
     def recommend(self, uid, top_k=10):
         # retrieval
-        candidates = candidate_generation(uid, self.meta, top_n=200)
+        if self.item_cf:
+            candidates = self.item_cf.recommend(uid, top_k=200)
+        else:
+            candidates = candidate_generation(uid, self.meta, top_n=200)
         feats = pd.DataFrame([build_feature_row(uid, iid, self.meta) for iid in candidates])
         # ranker
         scores = self.ranker.predict_proba(feats)
